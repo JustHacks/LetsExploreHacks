44423638ca54ce5fdb44114fd9733197b508e21a
Cow <69002970+SilentElapid@users.noreply.github.com>
2021-07-24 01:38:47 +0000

    Added nav + setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('pages/index');
+})
+
+app.listen(8080)
