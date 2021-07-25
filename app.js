9bc99293a31d7ba88e577350d1b82555cb038274
Cow <69002970+SilentElapid@users.noreply.github.com>
2021-07-25 07:00:58 +0000

    Commented partials + app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// Web-Server code
+
 const express = require('express');
 const app = express();
+
+// Set the view engine to EJS
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+
+// render the separate pages (also allowing for absolute urls)
 
 app.get('/', (req, res) => {
 	res.render('pages/index');
@@ -20,5 +26,7 @@ app.get('/ar', (req, res) => {
 app.get('/speechrec', (req, res) => {
 	res.render('pages/speechrec');
 })
+
+// Initiate the server on port 8080
 
 app.listen(8080)
